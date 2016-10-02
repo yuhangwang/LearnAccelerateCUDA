e810da89f87e8fa10ea246589d95cbf08cc6c4fc
Yuhang(Steven) Wang <stevenaura@live.com>
2016-10-01 20:00:58 -0500

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # LearnAccelerateCUDA
 Learn how to use Haskell package Accelerate-cuda
+
+#### note:
+Here is the dilemma: to build accelerate-cuda,
+you need ghc-7 not ghc-8. However, only ghc-8
+has the bug-fixed ld.exe. 
+Thus to you have to follow the suggestion 
+from stack and replace the ld.exe
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,17 @@
 module Main where
+import Data.Array.Accelerate      as Acc
+import qualified Data.Array.Accelerate.CUDA     as CUDA
+
+dotp :: Acc.Vector Float -> Acc.Vector Float -> Acc.Acc (Acc.Scalar Float)
+dotp xs ys = let xs' = Acc.use xs
+                 ys' = Acc.use ys
+             in
+             Acc.fold (+) 0 (Acc.zipWith (*) xs' ys')
 
 main :: IO ()
 main = do
+  let xs = Acc.fromList (Acc.Z :. 5) [0..] :: Acc.Vector Float
+  let ys = Acc.fromList (Acc.Z :. 5) [0..] :: Acc.Vector Float
+  let zs = dotp xs ys
+  print zs
   putStrLn "hello world"
